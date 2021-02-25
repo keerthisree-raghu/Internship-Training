09f1859ec5a5ad42d6118403a1225af744fbff2e
Keerthisree <51854360+keerthisree-raghu@users.noreply.github.com>
2021-02-25 23:28:55 +0530

    Delete Q5.java

--- src/Q5.java
+++
@@ -1,9 +0,0 @@
-package InterfacesExercises;
-
-// Create an interface with 3 methods in a package. Implement this in a class in different package.
-
-public interface Q5 {
-	public void foo();
-	public void bar();
-	public void baz();
-}
