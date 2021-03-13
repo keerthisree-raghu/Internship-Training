3eadbf9915a9ffec41aed70ea79e2279b51d03d1
Keerthisree <51854360+keerthisree-raghu@users.noreply.github.com>
2021-03-13 22:32:39 +0530

    Delete src/week4_tasks directory

--- src/week4_tasks/XMLtoExcel/TestXMLtoExcel.java
+++
@@ -1,8 +0,0 @@
-package week4_tasks.XMLtoExcel;
-
-public class TestXMLtoExcel {
-	public static void main(String[] args) throws Exception {
-		XMLtoExcelConverter xmlToExcel = new XMLtoExcelConverter();
-		xmlToExcel.convertXMLtoExcel(args[0]);
-	}
-}
--- src/week4_tasks/XMLtoPDF/TestXMLtoPDF.java
+++
@@ -1,9 +0,0 @@
-package week4_tasks.XMLtoPDF;
-
-public class TestXMLtoPDF {
-	public static void main(String[] args) throws Exception {
-		XMLtoPDFConverter xmlToPDF = new XMLtoPDFConverter();
-		xmlToPDF.convertXMLtoPDF(args[0]);
-//		xmlToPDF.convertXMLtoPDF("C:/workspace/EYInternshipTraining/invoice.xml");
-	}
-}
